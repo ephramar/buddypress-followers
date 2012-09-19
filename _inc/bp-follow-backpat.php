fc28f61389e7eddbb8c557b6e24bdcb1c5750ceb
Van Lauren Laffressia <krzysztof@philker.ph>
2012-09-19 15:31:28 +0800

    Added additional widgets
    Added "Most Followed User" and "Most Followed Artist" widgets

---
+++ _inc/bp-follow-backpat.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Backwards compatibililty functions for BP 1.2.x.
+ *
+ * @author r-a-y
+ * @package BP-Follow
+ * @subpackage Backpat
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/** BACKPAT FUNCTIONS ************************************************/
+
+if ( !function_exists( 'bp_actions' ) ) :
+function bp_actions() {
+	do_action( 'bp_actions' );
+}
+add_action( 'wp', 'bp_actions', 3  );
+endif;
+
+if ( !function_exists( 'bp_is_current_component' ) ) :
+function bp_is_current_component( $slug ) {
+	global $bp;
+
+	if ( $bp->current_component == $slug )
+		return true;
+
+	return false;
+}
+endif;
+
+if ( !function_exists( 'bp_is_current_action' ) ) :
+function bp_is_current_action( $action = '' ) {
+	global $bp;
+
+	if ( $action == $bp->current_action )
+		return true;
+
+	return false;
+}
+endif;
+
+if ( !function_exists( 'bp_is_user' ) ) :
+function bp_is_user() {
+	return bp_is_member();
+}
+endif;
+
+if ( !function_exists( 'bp_get_user_meta_key' ) ) :
+function bp_get_user_meta_key( $key = false ) {
+	return apply_filters( 'bp_get_user_meta_key', $key );
+}
+endif;
+
+if ( !function_exists( 'bp_get_user_meta' ) ) :
+function bp_get_user_meta( $user_id, $meta_key, $single = false ) {
+	return get_user_meta( $user_id, bp_get_user_meta_key( $meta_key ), $single );
+}
+endif;
+
+if ( !function_exists( 'bp_update_user_meta' ) ) :
+function bp_update_user_meta( $user_id, $meta_key, $meta_value, $prev_value = '' ) {
+	return update_user_meta( $user_id, bp_get_user_meta_key( $meta_key ), $meta_value, $prev_value );
+}
+endif;
+
+if ( !function_exists( 'bp_core_delete_notifications_by_type' ) ) :
+function bp_core_delete_notifications_by_type( $user_id, $component_name, $component_action ) {
+	return BP_Core_Notification::delete_for_user_by_type( $user_id, $component_name, $component_action );
+}
+endif;
+
+if ( !function_exists( 'bp_update_is_item_admin' ) ) :
+function bp_update_is_item_admin( $is_item_admin = false, $component = '' ) {
+	global $bp;
+
+	if ( empty( $component ) )
+		$component = bp_current_component();
+
+	$bp->is_item_admin = apply_filters( 'bp_update_is_item_admin', $is_item_admin, $component );
+}
+endif;
+
+?>
---
+++ _inc/bp-follow-widgets-display.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * BP Follow Display Top User Widgets
+ *
+ * @package BP-Follow
+ * @subpackage Widgets
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Add a "Most followed user" widget for the logged-in user
+ *
+ * @subpackage Widgets
+ */
+class BP_Top_Follower_Widget extends WP_Widget {
+	function bp_top_follower_widget() {
+		parent::WP_Widget( false, $name = __( "Most Followed User", 'bp-follow' ) );
+	}
+
+	function widget( $args, $instance ) {
+
+		extract( $args );
+		if( !$q = BP_Follow::get_top_user() ) {
+			return false;
+		}
+		
+		if( !empty( $q ) == true ) {
+
+			do_action( 'bp_before_following_widget' );
+
+			echo $before_widget;
+			echo $before_title
+			   . __( 'Most followed user', 'bp-follow' )
+			   . $after_title; ?>
+
+			<div class="avatar-block">
+				<?php foreach( $q as $k ): ?>
+					<?php foreach( $k as $top_user ): ?>
+						<div class="item-avatar">
+							<a href="<?php echo get_bloginfo('url') . '/members/' . $top_user->user_nicename . '/profile'; ?>"><?php echo get_avatar( $top_user->leader_id, 50 ); ?></a>
+						</div>
+					<?php endforeach; ?>
+				<?php endforeach; ?>
+			</div>
+
+			<?php echo $after_widget; ?>
+
+			<?php do_action( 'bp_after_following_widget' ); ?>
+	
+	<?php }
+	}
+
+}
+add_action( 'widgets_init', create_function( '', 'return register_widget("BP_Top_Follower_Widget");' ) );
+
+?>
